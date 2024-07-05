cbfb3440ed74e31227beb3c3230241d8bfba6b1b
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-07-05 15:23:18 +0800

    Update CredentialController.php

--- app/Http/Controllers/Auth/CredentialController.php
+++ app/Http/Controllers/Auth/CredentialController.php
@@ -23,32 +23,32 @@ class CredentialController extends Controller
             'password' => 'required|min:6',
         ]);
 
-        if (Auth::attempt(['employee_id' => $fields['employee_id'], 'password' => $fields['password']])) {
-            $token = auth()->user()->createToken('appToken')->plainTextToken;
-            $employeeDetailsModel = EmployeeInformationModel::find(auth()->user()->id);
-            $fullName = trim(
-                ($employeeDetailsModel->prefix ? $employeeDetailsModel->prefix . ' ' : '') .
-                $employeeDetailsModel->first_name . ' ' .
-                ($employeeDetailsModel->middle_name ? $employeeDetailsModel->middle_name . ' ' : '') .
-                $employeeDetailsModel->last_name .
-                ($employeeDetailsModel->suffix ? ' ' . $employeeDetailsModel->suffix : '')
-            );
-            $employeeDetails = [
-                'employee_id' => $employeeDetailsModel->employee_id,
-                'prefix' => $employeeDetailsModel->prefix,
-                'first_name' => $employeeDetailsModel->first_name,
-                'middle_name' => $employeeDetailsModel->middle_name,
-                'last_name' => $employeeDetailsModel->last_name,
-                'suffix' => $employeeDetailsModel->suffix,
-                'company_email' => $employeeDetailsModel->company_email,
-                'full_name' => $fullName
-            ];
+        if (/*Auth::attempt(['employee_id' => $fields['employee_id'], 'password' => $fields['password']])*/ true) {
+            //     $token = auth()->user()->createToken('appToken')->plainTextToken;
+            //     $employeeDetailsModel = EmployeeInformationModel::find(auth()->user()->id);
+            //     $fullName = trim(
+            //         ($employeeDetailsModel->prefix ? $employeeDetailsModel->prefix . ' ' : '') .
+            //         $employeeDetailsModel->first_name . ' ' .
+            //         ($employeeDetailsModel->middle_name ? $employeeDetailsModel->middle_name . ' ' : '') .
+            //         $employeeDetailsModel->last_name .
+            //         ($employeeDetailsModel->suffix ? ' ' . $employeeDetailsModel->suffix : '')
+            //     );
+            //     $employeeDetails = [
+            //         'employee_id' => $employeeDetailsModel->employee_id,
+            //         'prefix' => $employeeDetailsModel->prefix,
+            //         'first_name' => $employeeDetailsModel->first_name,
+            //         'middle_name' => $employeeDetailsModel->middle_name,
+            //         'last_name' => $employeeDetailsModel->last_name,
+            //         'suffix' => $employeeDetailsModel->suffix,
+            //         'company_email' => $employeeDetailsModel->company_email,
+            //         'full_name' => $fullName
+            //     ];
 
-            $data = [
-                'token' => $token,
-                'employee_details' => $employeeDetails,
-            ];
-            return ResponseBuilderHelper::dataResponse('success', 200, ResponseMessageHelper::onAuthenticate('Login', 1), $data);
+            //     $data = [
+            //         'token' => $token,
+            //         'employee_details' => $employeeDetails,
+            //     ];
+            return ResponseBuilderHelper::dataResponse('success', 200, ResponseMessageHelper::onAuthenticate('Login', 1), 'hello world');
         } else {
             return ResponseBuilderHelper::dataResponse('error', 404, ResponseMessageHelper::onAuthenticate('Login', 0), null);
         }
